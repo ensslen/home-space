fc3210eadfc9859761f58e2d097f786d49972317
Andrew Tipton <andrew@kiwidrew.com>
2017-07-29 15:17:54 +1200

    Forgot format=unaligned so the JSON data came out corrupted, oops.

--- db/export_json.sh
+++ db/export_json.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-psql service=govhack --tuples-only -c 'select array_to_json(array_agg(row_to_json(trademe)),TRUE) from trademe' -o data.json
+psql service=govhack -P 'format=unaligned' --tuples-only -c 'select array_to_json(array_agg(row_to_json(trademe)),TRUE) from trademe' -o data.json
