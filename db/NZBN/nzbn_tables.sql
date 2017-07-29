4bc580310d7493d580d587574f17dc275a2bc548
steven <steven@stevenensslen.com>
2017-07-30 09:55:33 +1200

    adds table ddl

---
+++ db/NZBN/nzbn_tables.sql
@@ -0,0 +1,12 @@
+create table nzbn_business_new
+(nzbn bigint
+,name varchar(1000)
+);
+
+
+create table nzbn_directors_new
+(nzbn bigint
+,first_name varchar(1000)
+,middle_name varchar(1000)
+,last_name varchar(1000)
+);
