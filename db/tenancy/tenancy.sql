5e1c3e3da58c8aac50620b805c07eb4465b8842f
steven <steven@stevenensslen.com>
2017-07-30 13:18:27 +1200

    inserts into association table

--- db/tenancy/tenancy.sql
+++ db/tenancy/tenancy.sql
@@ -1,5 +1,5 @@
 create table tenancy
-(	 id varchar(100)
+(	 id varchar(100) primary key
 	,applicationId  text
 	,applicationNumber  text 
 	,casePerOrg  text
@@ -23,6 +23,13 @@ create table tenancy
 	,tenancySuburb  text 
 	,tenancyUnitIdentifier_s text
 );
+
+create table trademe_tenancy
+(listing_id int
+,tenancy_id varchar(100)
+,primary key (listing_id, tenancy_id)
+);
+
 
 
 DROP FUNCTION IF EXISTS geomatch_tenancy_score(TRADEME, tenancy);
@@ -54,23 +61,18 @@ BEGIN
 END;
 $$;
 
-
-
-INSERT INTO trademe_addr (listing_id, address_id)
+INSERT INTO trademe_tenancy (listing_id, tenancy_id)
 WITH matches AS (
     -- Join to addresses using street name, then score each candidate
-    SELECT listing_id, address_id, geomatch_score(tm, addr) AS score,
-           row_number() OVER (PARTITION BY listing_id ORDER BY geomatch_score(tm, addr) DESC) AS ranking
-    FROM trademe tm 
-    JOIN tenancy t 
+    SELECT listing_id, applicationNumber, geomatch_tenancy_score(tm, t) AS score,
+           row_number() OVER (PARTITION BY listing_id ORDER BY geomatch_tenancy_score(tm, t) DESC) AS ranking
+    FROM trademe tm
+    JOIN tenancy t
     ON (canonical_street(tm.street) = t.tenancystreetname AND
         house_digits(tm.house) = house_digits(t.tenancystreetnumber))
 )
 -- Use a window query to select the best-ranked address point for each listing
-SELECT listing_id, address_id
+SELECT listing_id, id
 FROM matches
 WHERE ranking = 1
 ;
-
-
-
